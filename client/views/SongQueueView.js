aae33b612513a8e065c6d6ac2e6d48b5ed5c1e3e
kaljr <kenny.langston@gmail.com>
2015-04-08 16:27:29 -0700

    Add song enqueing, partially implement song queue view

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,10 +1,21 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.collection.on('change', this.render, this)
   },
 
   render: function() {
+
+    this.collection.forEach(SongQueueEntryView, function() {});
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render()
+      })
+    );
+
     return this.$el;
   }
 
